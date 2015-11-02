6469ed5dfaef63ec06591c3a04bf0b284c4b1acd
Ruslan Kovtun <koutoftimer@gmail.com>
2015-11-02 20:02:18 +0200

    Resolve issue with processing script, style tags

--- habraproxy.py
+++ habraproxy.py
@@ -1,4 +1,6 @@
 # ~*~ encoding: utf-8 ~*~
+from __future__ import unicode_literals
+
 import argparse
 import bs4
 import re
@@ -14,26 +16,52 @@ app.DOMAIN = None
 
 
 ALLOWED_CONTENT_TYPES = ('text/html', 'text/xml', 'text/xhtml', 'text/plain')
+DISALLOWED_TAGS = {'script', 'style'}
 WORD_RE = re.compile(r'(?P<prefix>^|\W)(?P<word>\w{6})(?P<suffix>$|\W)',
                      re.UNICODE)
-WORD_REPLACEMENT = u'\g<prefix>\g<word>™\g<suffix>'
+WORD_REPLACEMENT = r'\g<prefix>\g<word>™\g<suffix>'
 
 
 def html_content_type(content_type):
     """
     Determine whether `content_type` is subset of xml or plain text.
 
-    :type content_type: str
+    :type content_type: six.text_type
     :rtype: bool
     """
     return any(map(content_type.count, ALLOWED_CONTENT_TYPES))
 
 
+def allowed(text_node):
+    """
+    Determine whether current node allowed for processing. Javascript and css
+    stylesheets should not be processed as well as comments and other
+    declarative tags.
+
+    :type text_node: bs4.element.NavigableString
+    :rtype: bool
+    """
+    return (
+        not isinstance(text_node, bs4.element.PreformattedString)
+        and text_node.name not in DISALLOWED_TAGS
+        and not any(map(lambda parent: parent.name in DISALLOWED_TAGS,
+                        text_node.parents))
+    )
+
+
 def replace(text):
+    """
+    Takes markup of html page and return markup with processed text nodes.
+
+    Note: it tries to fix wrong markup using html5lib.
+
+    :type text: six.text_type
+    :rtype: six.text_type
+    """
     html = bs4.BeautifulSoup(text, 'html5lib')
-    for line in html.find_all(text=True):
-        if not isinstance(line, bs4.element.PreformattedString):
-            line.replace_with(WORD_RE.sub(WORD_REPLACEMENT, line))
+    for text_node in html.find_all(text=True):
+        if allowed(text_node):
+            text_node.replace_with(WORD_RE.sub(WORD_REPLACEMENT, text_node))
     return html.prettify(formatter='html')
 
 
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 # -*- encoding: utf-8 -*-
+from __future__ import unicode_literals
+
 from habraproxy import replace
 
 
@@ -18,6 +20,62 @@ HTML_WITH_DOCTYPE = '''<!DOCTYPE html>
 </html>'''
 
 
+HTML_WITH_SCRIPT_TAG = '''<html lang="en">
+ <head>
+  <script>
+   Math.random()
+  </script>
+ </head>
+ <body>
+ </body>
+</html>'''
+
+
+HTML_WITH_STYLE_TAG = '''<html lang="en">
+ <head>
+  <style>
+   button { height: 40px; }
+  </style>
+ </head>
+ <body>
+ </body>
+</html>'''
+
+
+HTML_WITH_COMMENT_TAG = '''<html lang="en">
+ <head>
+  <!--[if IE]>
+   <link rel="stylesheet" type="text/css" href="all-ie-only.css" />
+  <![endif]-->
+ </head>
+ <body>
+ </body>
+</html>'''
+
+
+HTML_WITH_BUTTON_TAG = '''<html lang="en">
+ <head>
+ </head>
+ <body>
+  <button>
+   Send
+  </button>
+ </body>
+</html>'''
+
+
 class TestReplace(object):
     def test_doctype(self):
         assert replace(HTML_WITH_DOCTYPE) == HTML_WITH_DOCTYPE
+
+    def test_script_tag(self):
+        assert replace(HTML_WITH_SCRIPT_TAG) == HTML_WITH_SCRIPT_TAG
+
+    def test_style_tag(self):
+        assert replace(HTML_WITH_STYLE_TAG) == HTML_WITH_STYLE_TAG
+
+    def test_comment_tag(self):
+        assert replace(HTML_WITH_COMMENT_TAG) == HTML_WITH_COMMENT_TAG
+
+    def test_button_tag(self):
+        assert replace(HTML_WITH_BUTTON_TAG) == HTML_WITH_BUTTON_TAG
