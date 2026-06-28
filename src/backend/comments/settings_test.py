0b55c685258f4b1a68e4c1404ff7679cf76b793a
Ruslan Kovtun <koutoftimer@gmail.com>
2026-06-28 14:56:00 +0300

    Improves test speed
    Uses inmemory sqlite DB for test and MD5 hashing function to speed up
    test user creation time.

---
+++ src/backend/comments/settings_test.py
@@ -0,0 +1,12 @@
+from .settings import *  # noqa
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
